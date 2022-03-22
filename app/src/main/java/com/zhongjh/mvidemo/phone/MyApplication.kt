c0002f9013d54601f63759b695bdd636ad7f7e4b
zhongjh <254191389@qq.com>
2022-03-22 18:04:55 +0800

    完善框架

---
+++ app/src/main/java/com/zhongjh/mvidemo/phone/MyApplication.kt
@@ -0,0 +1,10 @@
+package com.zhongjh.mvidemo.phone
+
+/**
+ *
+ * @author zhongjh
+ * @date 2022/3/22
+ * 代码规范：https://github.com/getActivity/AndroidCodeStandard
+ */
+class MyApplication {
+}
--- mvilibrary/src/main/java/com/zhongjh/mvilibrary/base/BaseApplication.kt
+++ mvilibrary/src/main/java/com/zhongjh/mvilibrary/base/BaseApplication.kt
@@ -1,13 +1,40 @@
 package com.zhongjh.mvilibrary.base
 
+import android.app.Application
+import java.lang.IllegalStateException
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
 /**
  * Application基类
  *
  * @author zhongjh
  * @date 2022/3/22
  */
-class BaseApplication {
+class BaseApplication : Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
 
+    companion object {
+        private var instance: BaseApplication by NotNullSingleValue()
+    }
+
+    private class NotNullSingleValue<T> : ReadWriteProperty<Any?, T> {
+        private var value: T? = null
+        override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+            return value ?: throw IllegalStateException("application not initialized")
+        }
+
+        override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+            if (this.value == null) {
+                this.value = value
+            } else {
+                throw IllegalStateException("application already initialized")
+            }
+        }
+    }
 
 }
